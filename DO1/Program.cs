fb6e17465f5e02008aae024b948bca83ec6b2d4a
yarrobong <oneboardsholi@gmail.com>
2023-09-14 22:17:54 +0500

    Добавьте файлы проекта.

---
+++ DO1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите координаты точки A: ");
+float x1 = float.Parse(Console.ReadLine());
+float y1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B: ");
+float x2 = float.Parse(Console.ReadLine());
+float y2 = float.Parse(Console.ReadLine());
+
+float deltaX = (x2 - x1);
+float deltaY = (y2 - y1);
+
+Double lengthAB = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+
+Console.WriteLine($"Длина отрезка AB = { lengthAB}");
